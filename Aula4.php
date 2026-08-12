2aacb06e9b2d1a1cfbc718c36d2f635ec0ebaf97
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-08-11 21:17:13 -0300

    if else e for while

---
+++ Aula4.php
@@ -0,0 +1,12 @@
+<?php
+
+for($i = 5; $i >= 0; $i--) {
+    echo ($i) . "<br>";
+}
+
+$contador = 5;
+while ($contador >= 0) {
+    echo $contador . "<br>";
+
+    $contador--;
+}
---
+++ Ex1A3.php
@@ -0,0 +1,24 @@
+<?php
+
+$nota = 7;
+
+switch ($nota) {
+    case $nota >= 7;
+        echo "Aprovado ";
+        break;
+    case $nota >= 5;
+        echo "Recuperação";
+        break;
+    default:
+        echo "Reprovado";
+        break;
+} 
+
+$perfil = "Admin";
+
+if ($perfil == "Admin") {
+    echo("ADM");
+
+} else {
+    echo("Not ADM");
+}
