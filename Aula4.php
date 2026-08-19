4afc86e2ddb2bb45bf05ec12cf04b4d666f8cc0e
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-08-18 21:55:48 -0300

    Terminar em casa

--- Aula4.php
+++ Aula4.php
@@ -10,3 +10,26 @@ while ($contador >= 0) {
 
     $contador--;
 }
+
+for ($i = 10; $i > 0; $i--) {
+    $tabuada = 5 * $i ;
+    $saida = "5 x $i = $tabuada";
+    echo $saida;
+    echo "<br>";    
+}
+for ($x = 50; $x > 1; $x --) {
+    $Descobrir = $x % 2;
+    if ($Descobrir == 0) {
+        echo "<br>"; 
+        echo ($x);
+    }
+}
+
+for ($y = 20; $y > 1; $y --) {
+    $PrimeiraEtapa = $x % 2 ;
+    $SegundaEtapa = $x % 3 ;
+    if ($PrimeiraEtapa != 0) {
+    } else if ($SegundaEtapa != 0) {
+         echo $y;
+    }
+}
