83dc0f9e9cc5c83fccc7b86677eada8abbe1e151
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-09-22 20:52:00 -0300

    Novo Backup

--- dump-infoserv-202609222049.sql
+++ dump-infoserv-202609222049.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: localhost    Database: Infoserv
+-- Host: localhost    Database: infoserv
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.6.23-MariaDB-0ubuntu0.22.04.1
 
@@ -29,7 +29,10 @@ CREATE TABLE `funcionario` (
   `cracha` varchar(50) NOT NULL,
   `sobrenome` varchar(100) DEFAULT NULL,
   `setor` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id_pessoa` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `funcionario_pessoa_FK` (`id_pessoa`),
+  CONSTRAINT `funcionario_pessoa_FK` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,12 +42,38 @@ CREATE TABLE `funcionario` (
 
 LOCK TABLES `funcionario` WRITE;
 /*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
-INSERT INTO `funcionario` VALUES (1,'Vitor',1500,'201','Alves','sasas');
+INSERT INTO `funcionario` VALUES (1,'Vitor',1500,'201','Alves','sasas',1);
 /*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping routines for database 'Infoserv'
+-- Table structure for table `pessoa`
+--
+
+DROP TABLE IF EXISTS `pessoa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pessoa` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa`
+--
+
+LOCK TABLES `pessoa` WRITE;
+/*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
+INSERT INTO `pessoa` VALUES (1,'Vitor','Exemplo@gmail.com',123456789),(2,'Teste 2','Exemplo2@gmail.com',123456799),(3,'Teste 3','Exemplo3@gmail.com',1223456799);
+/*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'infoserv'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-09-08 21:49:32
+-- Dump completed on 2026-09-22 20:49:56
